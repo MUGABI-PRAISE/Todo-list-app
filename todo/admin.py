e5ca99f6360170151cd6da92f7f3957b820ab207
mugabi praise <mugabipraise4@gmail.com>
2025-02-26 19:13:07 +0300

    migrations

--- todo/admin.py
+++ todo/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from.models import Task
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('description', 'completed', 'created_at', 'updated_at')
+    list_display = ('description', 'completed', 'created_at', 'updated_at', 'user')
     #list_filter = ('completed', 'created_at', 'updated_at')
     search_fields = ('description',)
     ordering = ('-created_at',)
